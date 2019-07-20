23f436a99533df97b7a11f41532e5bb6388974ef
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-07-20 10:11:05 +0700

    intro flask api restful

--- Basic Flask API Restful/app.py
+++ Basic Flask API Restful/app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class User(Resource):
-	def get(self):
-		return {'name': 'riffadi'}
+users = {}
 
-api.add_resource(User, '/user')
+class User(Resource):
+	def get(self, user_id):
+		return {'name': users[user_id]}
+	def put(self, user_id):
+		users[user_id] = request.form['user']
+		return {'name' : users[user_id]}
+
+api.add_resource(User, '/user/<string:user_id>')
 
 if __name__ == '__main__':
 	app.run(debug=True)

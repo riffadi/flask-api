6e7670ea95242865fe1ad74affe78a1355f0f023
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-08-10 14:51:07 +0700

    sign up dan sign in, generate access token jwt, access protection page

--- Basic Flask API Restful/app.py
+++ Basic Flask API Restful/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
-
 from resources.messages import messages_api
 from resources.users import users_api
+
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 import models
 
@@ -13,7 +17,9 @@ app = Flask(__name__)
 app.register_blueprint(messages_api, url_prefix  = '/api/v1')
 app.register_blueprint(users_api, url_prefix  = '/api/v1')
 
-
+#ACCESS_TOKEN_JWT
+app.config['SECRET_KEY'] = 'reandomString_superSecret31434khkafbak'
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
 	models.initialize()
--- Basic Flask API Restful/resources/users.py
+++ Basic Flask API Restful/resources/users.py
@@ -1,15 +1,85 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from resources.messages import messages_api
+from flask import jsonify, Blueprint, abort
+from flask_restful import Resource,Api, reqparse, fields, marshal, marshal_with
+from hashlib import md5
+
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+
 
 import models
 
+user_fields = {
+	'username' : fields.String,
+	'access_token' : fields.String,
+}
 
-class  user:
-	pass
+class UserBase(Resource):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument(
+			'username',
+			required=True,
+			help = 'Username wajib ada',
+			location = ['form', 'json']
+		)
+		self.reqparse.add_argument(
+			'password',
+			required=True,
+			help = 'Password wajib ada',
+			location = ['form', 'json']
+		)
+		super().__init__()
+
+
+class UserList(UserBase):
+
+	def post(self):
+		args = self.reqparse.parse_args()
+		username = args.get('username')
+		password = args.get('password')
+		try:
+			models.User.select().where(models.User.username == username).get()
+		except models.User.DoesNotExist:
+			user = models.User.create(
+				username = username,
+				password = md5(password.encode('utf-8')).hexdigest()
+			)
+			#send token
+			access_token = create_access_token(identity = username)
+			user.access_token = access_token
+			return marshal(user, user_fields)
+		else:
+			raise Exception('Username sudah terdaftar')
+
+	@jwt_required
+	def get(self):
+		return {'message' : 'ini bagian yang terproteksi'}
+
+
+class User(UserBase):
+
+
+	def post(self):
+		args = self.reqparse.parse_args()
+		username = args.get('username')
+		password = args.get('password')
+		try:
+			hashPass = md5(password.encode('utf-8')).hexdigest()
+			user = models.User.get((models.User.username == username) &
+						 (models.User.password == hashPass))
+		except models.User.DoesNotExist:
+			return {'messages' : 'User or Password is wrong!'}
+		else:
+			access_token = create_access_token(identity = username)
+			return {'messages' : 'You have successfully signin ', 'access_token': access_token}
+
 
 
 users_api = Blueprint('resources.users', __name__)
-api = Api(messages_api)
+api = Api(users_api)
 
-api.add_resource(Users, '/users', endpoint='users')
+api.add_resource(UserList, '/user/register', endpoint='user/register')
+api.add_resource(User, '/user/signin', endpoint='user/signin')
+
